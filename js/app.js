b6d5a1aa288aeb31696f0d505df4698a6a75458a
Gabriel Lew <lew.gabe@gmail.com>
2015-08-11 23:33:09 -0700

    Makes it such that if you die you stay the same character

--- js/app.js
+++ js/app.js
@@ -74,7 +74,6 @@ Player.prototype.handleInput = function(key) {
 Player.prototype.die = function() {
     this.x = 202;
     this.y = 386;
-    Player.chooseRandomCharacter.call(this);
 }
 
 Player.chooseRandomCharacter = function() {
@@ -96,7 +95,7 @@ Player.chooseRandomCharacter = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 5; i++) {
     allEnemies.push(new Enemy());
 }
 var player = new Player();
